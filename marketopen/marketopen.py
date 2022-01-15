bb4a0ff24a1a7fa49f9a4fa4f1cf48086b3ffa20
mmcelhan <matthewmcelhaney@gmail.com>
2022-01-15 10:02:24 -0700

    fixed two holidays

--- marketopen/marketopen.py
+++ marketopen/marketopen.py
@@ -1,9 +1,40 @@
 import datetime
 
+"""
+Package that returns if it's a trading day on the US Stock market.
+
+Stock Market Holidays:
+
+  New Years
+  Martin Luther King Day
+  Presidents Day
+  Good Friday (not af Federal Holiday surprisingly)
+  Memorial Day
+  Independence Day
+  Labor Day
+  Thanksgiving
+  Christmas
+
+The market is open if the holiday falls on a Friday, but off the following Monday if it falls on a Sunday. Strange
+
+"""
+
 
 def market_open(date):
     date = strip_date(date)
     return MarketOpen(date).market_open
+
+
+def is_weekend(date):
+    """ returns True if day is on a weekend """
+    date = strip_date(date)
+    return MarketOpen(date).weekend
+
+
+def is_weekday(date):
+    """ returns True if day is on a weekday """
+    date = strip_date(date)
+    return not MarketOpen(date).weekend
 
 
 def next_market_open_date(date):
@@ -19,10 +50,7 @@ def next_market_open_date(date):
 
 
 def strip_date(dt):
-    """
-    param dt:  datetime
-    return: takes a datetime and only keeps the date
-    """
+    # takes a datetime and only keeps the date
     if isinstance(dt, datetime.datetime):
         dt = datetime.datetime(dt.year, dt.month, dt.day).date()
 
@@ -45,6 +73,8 @@ class MarketOpen:
         self.month_day = str(self.input_date.month) + '-' + str(self.input_date.day)  # get day and month
         self.weekday = self.input_date.weekday()  # get weekday
         self.market_open = True  # set market to open and set to false if exception occurs
+        self.weekend = False
+        self.is_weekend()
 
         function_list = ['is_weekend', 'is_not_new_years', 'is_not_mlk', 'is_not_presidents_day', 'is_not_good_friday',
                          'is_not_memorial_day', 'is_not_independence_day', 'is_not_labor_day', 'is_not_thanksgiving',
@@ -56,10 +86,11 @@ class MarketOpen:
     def is_weekend(self):
         if self.weekday in [5, 6]:
             self.market_open = False
+            self.weekend = True
 
     def is_not_christmas(self):
         if self.month_day == '12-25':
-            self.market_open = False  # Christmas day get some presents
+            self.market_open = False  # christmas day get some presents
         elif (self.weekday == 0) & (self.month_day == '12-26'):  # Monday the 26th of January
             self.market_open = False
         elif (self.weekday == 4) & (self.month_day == '12-24'):  # Friday the 24th of December
@@ -91,7 +122,7 @@ class MarketOpen:
     def is_not_good_friday(self):
         # boo lunar calendars
         good_friday_dates = ['2021-4-2', '2022-4-15', '2023-4-7', '2024-3-29', '2025-4-18', '2026-4-03',
-                             '2027-3-26', '2028-4-14', '2029-3-30',  '2030-4-19']
+                             '2027-3-26', '2028-4-14', '2029-3-30', '2030-4-19']
 
         day = str(self.input_date.year) + '-' + str(self.input_date.month) + '-' + str(self.input_date.day)
 
@@ -105,8 +136,12 @@ class MarketOpen:
             self.market_open = False
 
     def is_not_memorial_day(self):
+        # the last Monday in May
         dt = datetime.datetime(self.input_date.year, 6, 1)
-        delta_time = datetime.timedelta(days=(dt.weekday()))
+        if dt.weekday() != 0:
+            delta_time = datetime.timedelta(days=(dt.weekday()))
+        else:
+            delta_time = datetime.timedelta(days=7)
         memorial_day = dt - delta_time
         if self.input_date == memorial_day:
             self.market_open = False
@@ -133,12 +168,12 @@ class MarketOpen:
     def is_not_thanksgiving(self):
         dt = datetime.datetime(self.input_date.year, 11, 1)
 
-        if dt.weekday() == 3:
-            delta_time = datetime.timedelta(days=(0 - dt.weekday()))
+        if dt.weekday() >= 4:
+            delta_time = datetime.timedelta(days=(10 - dt.weekday()))  # go back to the first thursday
         else:
             delta_time = datetime.timedelta(days=(3 - dt.weekday()))
 
-        thanksgiving = dt + delta_time + datetime.timedelta(days=21)  # get the fourth Thursday of February
+        thanksgiving = dt + delta_time + datetime.timedelta(days=21)  # get the fourth Thursday of November
 
         if self.input_date == thanksgiving:
             self.market_open = False
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='marketopen',
-      version='1.0',
+      version='1.1',
       description='returns if the stock market is open given a day',
       url='https://lamplightlab.com',
       author='Matthew McElhaney',
